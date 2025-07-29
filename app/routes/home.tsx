714ab9a0df0e8cba30a0344b099c96a195b917f9
SHUJA <106554018+Shujaulislam@users.noreply.github.com>
2025-07-30 01:17:59 +0530

    Add resume dashboard components and types
    Introduces Navbar, ResumeCard, and ScoreCircle components for the resume dashboard UI. Replaces the previous welcome page and assets with a new home route that displays resume cards using sample data from constants. Adds type definitions for resumes and feedback, and updates meta information for the home page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback For your Dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1 className="page-heading">Track your Applications and Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered Feedback</h2>
+      </div>
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map(resume => (
+                <div>
+                  <ResumeCard key={resume.id} resume={resume} />
+                </div>
+            ))}
+          </div>
+      )}
+    </section>
+  </main>
 }
